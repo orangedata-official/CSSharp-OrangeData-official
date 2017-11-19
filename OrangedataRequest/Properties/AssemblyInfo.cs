68ae92fe620371523d0619e96908fb04e7035bad
Andrey Efimov <anothar@ya.ru>
2017-11-19 17:13:32 +0500

    Fix .NET version and nuget package

--- OrangedataRequest/Properties/AssemblyInfo.cs
+++ OrangedataRequest/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OrangedataRequest")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright OrangeData©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
